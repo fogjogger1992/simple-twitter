fff56f4c8aaf68aa921866e2e4672a61de4d448b
Leslie <lesliezsy12@gmail.com>
2021-07-05 17:59:31 +0800

    modify AdminSignIn UI; add LoginForm

--- src/views/AdminSignIn.vue
+++ src/views/AdminSignIn.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>AdminSignIn Page</h1>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="10" sm="8" md="6" lg="5" class="text-center">
+        <p class="text--h6 font-weight-bold mt-4">後台登入</p>
+
+        <LoginForm />
+
+        <div class="text-right">
+        <router-link class="text-subtitle-2 secondary--text font-weight-bold" to="/signin">前台登入</router-link>
+        </div>
+        
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: "AdminSignIn",
+  components: { LoginForm },
+  data: () => ({
+
+  }),
+  methods: {
+  },
+  computed: {
+
+  },
+};
+</script>
