90f5aa36875c03db27fabecfa46204f515b461ee
EJ Tseng <fogjogger1992@gmail.com>
2021-07-05 12:09:42 +0800

    Install  Vue Router and Vuetify
    Add 404 NotFound.vue as a test for both router and vuetify

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-main>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="headline font-weight-bold mt-5">404 Page Not Found</h1>
+      </v-col>
+    </v-row>
+  </v-main>
+</template>
