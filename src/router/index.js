9e24c8b8532d9c85137b19bfc3885af7232ebf4c
EJ Tseng <fogjogger1992@gmail.com>
2021-07-05 23:44:59 +0800

    Add root route
    path '/' redirect to '/signin'

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    name: 'root',
+    redirect: '/signin',
+  },
+  {
     path: '/signin',
     name: 'sign-in',
     component: SignIn,
