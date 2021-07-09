0994bc6c675cf697cda8330ac50557dcacbaab0f
EJ Tseng <fogjogger1992@gmail.com>
2021-07-09 12:50:23 +0800

    refactor: Make UserProfileCard as a component

--- src/views/User.vue
+++ src/views/User.vue
@@ -19,70 +19,13 @@
         </v-col>
       </v-row>
     </v-card>
-    <!-- profile card -->
-    <v-card tile flat class="pa-0 profilecard">
-      <!-- bg -->
-      <v-row height="200px" class="ma-0 pa-0">
-        <v-img height="200px" :src="user.background"></v-img>
-      </v-row>
-      <!-- avatar & btn -->
-      <v-row height="140px" class="ma-0 px-3 py-0 avatarnbtn">
-        <!-- avatar -->
-        <v-col cols="6" class="ma-0 pa-0">
-          <v-avatar
-            white
-            height="140px"
-            width="140px"
-            style="border: 5px solid white"
-          >
-            <v-img :src="user.image"></v-img>
-          </v-avatar>
-        </v-col>
-        <!-- btn -->
-        <v-col cols="6" class="d-flex ma-0 px-0 py-4 align-self-end">
-          <v-spacer></v-spacer>
-          <v-btn
-            outlined
-            rounded
-            large
-            color="primary"
-            class="font-weight-bold"
-          >
-            編輯個人資料
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
-    <!-- info -->
-    <v-card tile flat height="70px"></v-card>
-    <v-row class="ma-0 pa-3">
-      <v-col cols="12" class="ma-0 pa-0">
-        <h3>{{ user.name }}</h3>
-      </v-col>
-      <v-col sold="12" class="ma-0 pa-0">
-        <p class="subtitle-2 grey--text font-weight-normal my-0 pa-0">
-          @{{ user.account }}
-        </p>
-      </v-col>
-    </v-row>
-    <v-row no-gutters align="center" class="py-0 px-3 ma-0">
-      <p class="subtitle-2 font-weight-regular mr-1 my-0 pa-0">
-        {{ user.description }}
-      </p>
-    </v-row>
-    <!-- followings & followers -->
-    <v-row no-gutters align="center" class="pa-3 ma-0">
-      <p class="subtitle-2 font-weight-bold my-0 pa-0">
-        {{ followings.length }} 個
-      </p>
-      <p class="subtitle-2 grey--text font-weight-bold mr-1 my-0 pa-0">
-        跟隨中
-      </p>
-      <p class="subtitle-2 font-weight-bold my-0 pa-0">
-        {{ followers.length }} 位
-      </p>
-      <p class="subtitle-2 grey--text font-weight-bold my-0 pa-0">跟隨者</p>
-    </v-row>
+
+    <UserProfileCard
+      :user="user"
+      :tweets="tweets"
+      :followings="followings"
+      :followers="followers"
+    />
 
     <!-- tabs -->
     <v-tabs
@@ -104,19 +47,8 @@
   </v-container>
 </template>
 
-<style scoped>
-.profilecard {
-  position: relative;
-}
-.avatarnbtn {
-  z-index: 99;
-  position: absolute;
-  bottom: -70px;
-  width: 100%;
-}
-</style>
-
 <script>
+import UserProfileCard from "../components/UserProfileCard.vue";
 import TweetCard from "../components/TweetCard.vue";
 
 // remove this after integrating API
@@ -223,6 +155,7 @@ const dummyData = {
 export default {
   name: "User",
   components: {
+    UserProfileCard,
     TweetCard,
   },
   data() {
