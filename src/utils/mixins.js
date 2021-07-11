d10999cf78129350cf38d0b1258d27342cc2145d
EJ Tseng <fogjogger1992@gmail.com>
2021-07-11 17:07:58 +0800

    Add empty image filter

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -3,7 +3,15 @@ import moment from 'moment'
 export const fromNowFilter = {
   filters: {
     fromNow (datetime) {
-      return datetime ? moment(datetime).fromNow() : '-'
+      return datetime ? moment(datetime, "YYYY-MM-DD, h:mm:ss a").calendar() : '-'
+    }
+  }
+}
+
+export const emptyImageFilter = {
+  filters: {
+    emptyImage(src) {
+      return src || 'https://via.placeholder.com/800x800/FF6602/FFFFFF?text=No+Image'
     }
   }
 }
