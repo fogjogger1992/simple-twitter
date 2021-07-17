8c1db951d4cd82eaffe90d3cf24b83b506024933
EJ Tseng <fogjogger1992@gmail.com>
2021-07-17 15:56:30 +0800

    Add chatroom message, notification, and input

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="ma-0 pa-0">
     <v-row no-gutters>
-      <v-col cols="4" class="ma-0 px-0 d-flex flex-column userlistcol">
+      <v-col cols="12" md="4" class="ma-0 px-0 d-flex flex-column userlistcol">
         <v-card
           tile
           flat
@@ -19,7 +19,7 @@
           />
         </div>
       </v-col>
-      <v-col cols="8" class="ma-0 px-0 d-flex flex-column chatroomcol">
+      <v-col cols="12" md="8" class="ma-0 px-0 d-flex flex-column chatroomcol">
         <v-card
           tile
           flat
@@ -27,7 +27,44 @@
           style="border-bottom: thin solid rgba(0, 0, 0, 0.12)"
           >公開聊天室</v-card
         >
-        <!-- {{ Chatroom }} -->
+        <!-- chatroom -->
+        <div class="chatroom">
+          <ChatroomMessage />
+        </div>
+        <v-spacer></v-spacer>
+        <!-- input -->
+        <v-card
+          tile
+          flat
+          class="pa-3 align-center"
+          style="border-top: thin solid rgba(0, 0, 0, 0.12)"
+        >
+          <v-row class="pa-0 ma-0">
+            <v-col cols="11" class="pa-0 ma-0 align-self-center">
+              <v-textarea
+                v-model="message"
+                class="pa-0 ma-0"
+                hide-details
+                rounded
+                filled
+                auto-grow
+                rows="1"
+                label="輸入訊息..."
+              ></v-textarea>
+            </v-col>
+            <v-col
+              cols="1"
+              class="py-0 ma-0 px-2 align-self-center d-flex justify-center"
+            >
+              <v-icon
+                @click.prevent.stop=""
+                color="primary"
+                style="font-size: 25px"
+                >far fa-paper-plane</v-icon
+              >
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -42,19 +79,22 @@
   border-right: thin solid rgba(0, 0, 0, 0.12);
   height: 100vh;
 }
-.onlineuserlist {
+.onlineuserlist,
+.chatroom {
   overflow-y: scroll;
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
 }
 /* Hide scrollbar for Chrome, Safari and Opera */
-.onlineuserlist::-webkit-scrollbar {
+.onlineuserlist::-webkit-scrollbar,
+.chatroom::-webkit-scrollbar {
   display: none;
 }
 </style>
 
 <script>
 import ChatroomUserCard from "../components/ChatroomUserCard.vue";
+import ChatroomMessage from "../components/ChatroomMessage.vue";
 import { mapState } from "vuex";
 
 // TODO: remove after integrating api
@@ -86,10 +126,12 @@ export default {
   data() {
     return {
       onlineUsers: [],
+      message: "",
     };
   },
   components: {
     ChatroomUserCard,
+    ChatroomMessage,
   },
   computed: {
     ...mapState(["currentUser"]),
