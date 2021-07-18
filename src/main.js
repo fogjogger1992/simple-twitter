6954cbe954657f4d4f726411139124f29d8216dc
EJ Tseng <fogjogger1992@gmail.com>
2021-07-19 00:51:17 +0800

    chore: update api baseURL

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
     debug: true,
-    connection: SocketIO('http://localhost:3000'),
+    connection: SocketIO('https://simple-twitter-api-demo.herokuapp.com/api'),
     withCredentials: true,
     vuex: {
         store,
